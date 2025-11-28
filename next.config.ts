f8eeffd03ba8e7b406de224b122ce8e81d297d93
satya00089 <satyasubudhi089@gmail.com>
2025-11-29 00:11:00 +0530

    Add GitHub Actions workflow for deploying Next.js to GitHub Pages and configure Next.js for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your repo name is not the root domain, add basePath
+  // basePath: '/your-repo-name',
+  // assetPrefix: '/your-repo-name/',
 };
 
 export default nextConfig;
